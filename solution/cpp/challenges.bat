0dfcc0fd0738a85231aa2780bbf3efdf441e3981
vasilegrafu <vasilegrafu@gmail.com>
2018-10-08 21:33:36 +0300

    Cpp project organization

--- solution/cpp/challenges.bat
+++ solution/cpp/challenges.bat
@@ -2,4 +2,4 @@
 
 SET PATH=i:\Installations\mingw-w64\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;%PATH%
 
-i:\Installations\VSCode-win32-x64-1.27.1\Code.exe didactic.code-workspace
+i:\Installations\VSCode-win32-x64-1.27.2\Code.exe challenges.code-workspace
