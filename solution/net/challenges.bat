10fc97ec4d233459ea6b9c756eaabb3ea70dbbc0
Vasile Grafu <vasilegrafu@gmail.com>
2018-10-23 22:43:23 +0300

    Change VSCode Version

--- solution/net/challenges.bat
+++ solution/net/challenges.bat
@@ -1,5 +1,5 @@
 @echo off
 
-SET PATH=d:\__MyDocuments\Installations\dotnet-sdk-2.1.401-win-x64;%PATH%
+SET PATH=i:\Installations\dotnet-sdk-2.1.401-win-x64;%PATH%
 
-d:\__MyDocuments\Installations\VSCode-win32-x64-1.27.2\Code.exe challenges.code-workspace
+i:\Installations\VSCode-win32-x64-1.27.1\Code.exe challenges.code-workspace
