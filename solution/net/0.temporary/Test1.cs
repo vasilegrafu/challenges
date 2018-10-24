91662170c7395575f8c33b7792dd0e36e7847d13
vasilegrafu <vasilegrafu@gmail.com>
2018-10-25 00:52:22 +0300

    Intermediate commit ...

--- solution/net/0.temporary/Test1.cs
+++ solution/net/0.temporary/Test1.cs
@@ -16,46 +16,43 @@ namespace Practice
     {
         int nonDivisibleSubset(int k, int[] S) {
             var D = S.Select((p) => p % k)
-                     .OrderBy((p) => p)
                      .GroupBy((p) => p)
                      .ToDictionary((p) => p.Key, (e) => e.Count());
 
-            List<int> keys = D.Keys.ToList();
-            foreach(int s in keys)
+            int count = 0;
+            if(D.ContainsKey(0))
             {
-                if(D.ContainsKey(k-s))
-                {
-                    D[s]--;
-                    D[k-s]--;
-                }
+                count += 1;
             }
-
-            List<HashSet<int>> hashSetList = new List<HashSet<int>>();
-
-            for(int i = 0; i < S.Length - 1; i++)
+            for (int i = 1; i <= k/2; i++)
             {
-                HashSet<int> hashSet = new HashSet<int>();
-                hashSet.Add(S[i]);
-
-                for(int j = (i+1); j < S.Length; j++)
+                if(i == k - i)
                 {
-                    if(hashSet.All((p) => (p + S[j]) % k != 0))
-                    {
-                        hashSet.Add(S[j]);
-                    }
+                    count++;
+                    continue;
                 }
 
-                hashSetList.Add(hashSet);
+                int count1 = 0;
+                int count2 = 0;
+                if(D.ContainsKey(i))
+                {
+                    count1 = D[i];
+                }
+                if(D.ContainsKey(k-i))
+                {
+                    count2 = D[k-i];
+                }
+                count += Math.Max(count1, count2);
             }
-                       
-            return hashSetList.Select((p) => p.Count()).Max();
+            return count;
         }
 
         [TestCase()]
         public void Main()
         {
-            int m = nonDivisibleSubset(7, new int[] { 278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436 });
+            //int m = nonDivisibleSubset(7, new int[] { 278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436 });
             //int m = nonDivisibleSubset(5, new int[] { 770528134, 663501748, 384261537, 800309024, 103668401, 538539662, 385488901, 101262949, 557792122, 46058493 });
+            int m = nonDivisibleSubset(1, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 
             Console.WriteLine(m);
         }
