8425d4c719116b5532adbc0b15a5baff00dd6089
vasilegrafu <vasilegrafu@gmail.com>
2019-08-22 23:58:46 +0300

    Permutations1 in progress

--- solution.net/Practice/Permutations/Permutations.tests.cs
+++ solution.net/Practice/Permutations/Permutations.tests.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
 
+namespace Permutations
+{
+    [TestFixture]
+    public partial class PermutationsTest
+    {
+        private void TestCorrectness(Func<int[], IList<int[]>> generatePermutations, int[] a, long permutationsCount)
+        {
+            IList<int[]> permutations = generatePermutations(a);
+            long permutationsCount2 = permutations.Count();
+            Assert.IsTrue(permutationsCount2 == permutationsCount);
+        }
+
+        [TestCase()]
+        public void TestCorrectness()
+        {
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1 }, 1);
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1, 2 }, 2);
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1, 2, 3 }, 6);
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1, 2, 3, 4 }, 24);
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1, 2, 3, 4, 5 }, 120);
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1, 2, 3, 4, 5, 6 }, 720);
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1, 2, 3, 4, 5, 6, 7}, 5040);
+            TestCorrectness(Permutations1.GeneratePermutations, new int[] { 1, 2, 3, 4, 5, 6, 7, 8}, 40320);
+        }
+    }
+}
--- solution.net/Practice/Permutations/Permutations1.cs
+++ solution.net/Practice/Permutations/Permutations1.cs
@@ -1,1 +1,23 @@
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Permutations
+{
+    public partial class Permutations1
+    {
+        private static void GeneratePermutations(int[] a, int i, out IList<int[]> permutations) 
+        {
+            
+        }
+
+        public static IList<int[]> GeneratePermutations(int[] a) 
+        {
+            IList<int[]> permutations = new List<int[]>();
+            GeneratePermutations(a, 0, out permutations);
+
+        }
+    }
+}
