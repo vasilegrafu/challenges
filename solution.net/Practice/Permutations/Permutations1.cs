243e181cd58c37dd93cc22cb55a6a9c6dd5fb04d
vasilegrafu <vasilegrafu@gmail.com>
2019-09-11 20:36:43 +0300

    Work on generating permutations part 2

--- solution.net/Practice/Permutations/Permutations1.cs
+++ solution.net/Practice/Permutations/Permutations1.cs
@@ -2,21 +2,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Permutations
 {
     public partial class Permutations1
     {
-        private static void GeneratePermutations(int[] a, int i, out IList<int[]> permutations) 
+        private static void GeneratePermutations(int[] a, int s, IList<int[]> permutations) 
         {
-            permutations = null;
+            if(s == (a.Length - 1))
+            {
+                int[] a2 = new int[a.Length];
+                a.CopyTo(a2, 0);
+                permutations.Add(a2);
+            }
+
+            for(int i = s; i < a.Length; i++)
+            {
+                a[s] = a[i];
+                GeneratePermutations(a, i, permutations);
+                a[i] = a[s];
+            }
         }
 
         public static IList<int[]> GeneratePermutations(int[] a) 
         {
             IList<int[]> permutations = new List<int[]>();
-            GeneratePermutations(a, 0, out permutations);
+            GeneratePermutations(a, 0, permutations);
             return permutations;
         }
     }
